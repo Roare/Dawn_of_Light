1bba4fe6e0423067e426f3e6567ab2b0c1a3abbe
etaewdev <etaewdev@users.sourceforge.net>
2006-12-07 00:09:26 +0000

    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/branch/Dawn%20of%20Light%20-%20The%20Server@297 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ scripts/CustomScripts/Realm.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using DOL.GS.Keeps;
+using DOL.GS.PacketHandler;
+
+namespace DOL.GS.Scripts
+{
+	[CmdAttribute(
+		"&realm",
+		1,
+		"Show realm status", "/realm")]
+	public class RealmCommandHandler : AbstractCommandHandler, ICommandHandler 
+	{
+		public int OnCommand(GameClient client, string[] args)
+		{
+			AbstractGameKeep thidranki = KeepMgr.getKeepByID(129);
+			AbstractGameKeep orseo = KeepMgr.getKeepByID(5);
+
+			ArrayList list = new ArrayList();
+			list.Add(thidranki.Name + ": " + GlobalConstants.RealmToName((eRealm)thidranki.Realm));
+			list.Add("");
+			list.Add(orseo.Name + ": " + GlobalConstants.RealmToName((eRealm)orseo.Realm));
+			list.Add("");
+			list.Add("Darkness Falls: " + GlobalConstants.RealmToName((eRealm)thidranki.Realm));
+
+			client.Out.SendCustomTextWindow("Realm Status", list);
+			return 1;
+		}
+	}
+}
