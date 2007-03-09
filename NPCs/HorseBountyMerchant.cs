073ff026b8b76f022dbf8d0f9a4cb18a558a1e0f
etaewdev <etaewdev@users.sourceforge.net>
2007-03-09 03:13:11 +0000

    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/branch/Dawn%20of%20Light%20-%20The%20Server@468 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ NPCs/HorseBountyMerchant.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System;
+using DOL.Database;
+using DOL.Events;
+
+namespace DOL.GS.Scripts
+{
+	/// <summary>
+	/// Represents an in-game merchant
+	/// </summary>
+	public class GameBountyHorseMerchant : GameBountyMerchant
+	{	
+		private static ArrayList m_items = new ArrayList();
+
+		[ScriptLoadedEvent]
+		public static void OnScriptCompiled(DOLEvent e, object sender, EventArgs args)
+		{
+			ItemTemplate chestnutHorse = (ItemTemplate)GameServer.Database.FindObjectByKey(typeof(ItemTemplate), "brownstandardmount");
+			if (chestnutHorse != null)
+				m_items.Add(chestnutHorse);
+
+			ItemTemplate greyHorse = (ItemTemplate)GameServer.Database.FindObjectByKey(typeof(ItemTemplate), "greystandardmount");
+			if (greyHorse != null)
+				m_items.Add(greyHorse);
+
+			ItemTemplate spottedHorse = (ItemTemplate)GameServer.Database.FindObjectByKey(typeof(ItemTemplate), "spottedstandardmount");
+			if (spottedHorse != null)
+				m_items.Add(spottedHorse);
+
+			ItemTemplate chocolateHorse = (ItemTemplate)GameServer.Database.FindObjectByKey(typeof(ItemTemplate), "chocolatestandardmount");
+			if (chocolateHorse != null)
+				m_items.Add(chocolateHorse);
+
+			ItemTemplate blackHorse = (ItemTemplate)GameServer.Database.FindObjectByKey(typeof(ItemTemplate), "blackstandardmount");
+			if (blackHorse != null)
+				m_items.Add(blackHorse);
+
+			ItemTemplate silverHorse = (ItemTemplate)GameServer.Database.FindObjectByKey(typeof(ItemTemplate), "whitestandardmount");
+			if (silverHorse != null)
+				m_items.Add(silverHorse);
+
+			ItemTemplate palominoHorse = (ItemTemplate)GameServer.Database.FindObjectByKey(typeof(ItemTemplate), "palominostandardmount");
+			if (palominoHorse != null)
+				m_items.Add(palominoHorse);
+
+			/*ItemTemplate chestnutWarHorse = (ItemTemplate)GameServer.Database.FindObjectByKey(typeof(ItemTemplate), "brownheavymount");
+			if (chestnutWarHorse != null)
+				m_items.Add(chestnutWarHorse);
+
+			ItemTemplate greyWarHorse = (ItemTemplate)GameServer.Database.FindObjectByKey(typeof(ItemTemplate), "greyheavymount");
+			if (greyWarHorse != null)
+				m_items.Add(greyWarHorse);
+
+			ItemTemplate spottedWarHorse = (ItemTemplate)GameServer.Database.FindObjectByKey(typeof(ItemTemplate), "spottedheavymount");
+			if (spottedWarHorse != null)
+				m_items.Add(spottedWarHorse);
+
+			ItemTemplate bayWarHorse = (ItemTemplate)GameServer.Database.FindObjectByKey(typeof(ItemTemplate), "spottedheavymount");
+			if (bayWarHorse != null)
+				m_items.Add(bayWarHorse);*/
+		}
+
+		public GameBountyHorseMerchant()
+			: base()
+		{
+			m_tradeItems = new MerchantTradeItems("horsebountymerchant");
+			foreach (ItemTemplate item in m_items)
+				m_tradeItems.AddTradeItem(0, eMerchantWindowSlot.FirstEmptyInPage, item);
+		}
+	}
+}
