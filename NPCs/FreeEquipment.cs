280d1a17e590d2b9a42a79d85375c452dfa9a1a2
etaewdev <etaewdev@users.sourceforge.net>
2007-04-24 17:28:40 +0000

    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/branch/Dawn%20of%20Light%20-%20The%20Server@551 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- NPCs/FreeEquipment.cs
+++ NPCs/FreeEquipment.cs
@@ -10,7 +10,7 @@ namespace DOL.GS.Scripts
 {
 	public class FreeEquipment
 	{
-		public const int FREE_LEVEL = 19;
+		public const int FREE_LEVEL = 49;
 		private static GameNPC Albion_FreeEquipment = null;
 		private static GameNPC Midgard_FreeEquipment = null;
 		private static GameNPC Hibernia_FreeEquipment = null;
