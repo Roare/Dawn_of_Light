d527b7f94b222d5aa59e09bd032b8193c10578da
etaewdev <etaewdev@users.sourceforge.net>
2007-05-08 09:12:19 +0000

    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/branch/Dawn%20of%20Light%20-%20The%20Server@584 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- NPCs/PersonalMerchants.cs
+++ NPCs/PersonalMerchants.cs
@@ -15,7 +15,17 @@ namespace DOL.GS.Scripts
 
 	public class ConsignmentMerchant : GameNPC
 	{
+		public override bool Interact(GamePlayer player)
+		{
+			if (!base.Interact(player))
+				return false;
 
+			//0:05:27.359 S=>C 0x02 inventory update v182 (slots:0 bits:0x00 visibleSlots:0x00 preAction:0x05)
+			player.Out.SendInventoryItemsUpdate(0x05, null);
+			//0:05:27.375 S=>C 0x1E consignment merchant money (copper:0  silver:0  gold:0   mithril:0   platinum:0)
+
+			return true;
+		}
 	}
 }
 
